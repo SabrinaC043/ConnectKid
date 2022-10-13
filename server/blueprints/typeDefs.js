b1aed43feb6d32d7beee77c0bd15d410367a3d26
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2022-10-13 13:18:17 -0500

    merge from main

--- server/blueprints/typeDefs.js
+++ server/blueprints/typeDefs.js
@@ -1,76 +1,63 @@
-const { gql } = require("apollo-server");
+// const { gql } = require('apollo-server-express');
 
-const TypeDefs = gql`
-#
+// const typeDefs = gql`
+// #User login will track information about specific user and associated child(ren)
 
-type User {
-_id: ID!
-name: String!
-password: String!
-passwordLength: INT!
-email: String!
-}
+// type User {
+// _id: ID!
+// name: String!
+// password: String!
+// passwordLength: INT!
+// email: String!
+// }
 
-type Parent {
-_id:ID!
-name:String!
-age: INT
-gender: String
-}
-
-type Extroversion {
-   _id:ID!
-    scale:[INT]
-    rating: [STRING]
-}
+// type Parent {
+// _id:ID!
+// name:String!
+// age: INT
+// gender: String
+// extroversionScale:INT
+// childCount: [INT]
+// }
 
 
-type Child {
+// type Child {
 
-    _id: ID! 
-    childCount: [INT]
-    name: String! 
-    childAge: INT! 
-    gender: String!
+//     _id: ID! 
+//     name: String! 
+//     childAge: INT! 
+//     gender: String!
     
-    }
+//     }
 
-    
-type Cook {
-    name: String!
-    link: String!
-    time: [INT]
-    preparation: [String]
-}
+// type Event {
+// name: String!
+// location: [String]
+// time:[INT]
+// date: [String]
+// preparationTips: [String] 
+// }
 
-type Event {
-name: String!
-location: [String]
-time:[INT]
-date: [String]
-preparationTips: [String] 
-}
+// type Auth {
+//     token:ID! 
+//     user:(User:ID!): User
+// }
 
-type Auth {
-    token:ID! 
-    user:(User:ID!): User
-}
-
-type Query {
-    User: [User]!
-    User(user:ID):User
-}
+// type Query {
+//     user: [user]!
+//     user(userId:Id!):User
+// }
 
 
-type Mutation {
-    add(Userid: ID!, name: String!, password: String!,passwordLength: INT!,email: String!):Auth login (email:String!. password!):Auth
+// type Mutation {
+//     add(UserId: ID!, name: String!, password: String!,passwordLength: INT!,email: String!):Auth login (email:String!. password!):Auth
 
-    addChild( id: ID!, childCount: [INT],name: String! , childAge: INT!, gender: String!):Child
+//     addChild( id: ID!, childCount: [INT],name: String! , childAge: INT!, gender: String!):Child
 
-    removeUser(User:ID!):User
-    removeChild(id:ID!, name:String):Child
-}
+//     removeUser(User:ID!):User
+//     removeChild(id:ID!, name:String):Child
+// }
 
 
-`;
-module.exports = typeDefs;
+// `;
+// module.exports = typeDefs;
