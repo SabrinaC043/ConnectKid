8fe823e7c4e9dea2a13b3954da37c3ff3adc1461
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-10-12 20:55:55 -0500

    Committing to pull to another device

--- client/src/components/cards/featureCard.js
+++ client/src/components/cards/featureCard.js
@@ -6,17 +6,47 @@ import { Container } from "react-bootstrap";
 const FeatureCard = (props) => {
     return(
 
-    <Card className="text-center col-9 border" style={{marginTop: "200px"}}>
-    <Card.Body>
-        <Card.Title><u>{props.title}</u></Card.Title>
-        <Card.Subtitle>{props.date}</Card.Subtitle>
-        <Card.Text>{props.time}</Card.Text>
+    // <Card className="text-center col-9 border" style={{marginTop: "200px"}}>
+    // <Card.Body>
+    //     <Card.Title><u>{props.title}</u></Card.Title>
+    //     <Card.Subtitle>{props.date}</Card.Subtitle>
+    //     <Card.Text>{props.time}</Card.Text>
 
-        <Card.Text>{props.text}</Card.Text>
+    //     <Card.Text>{props.text}</Card.Text>
  
-    </Card.Body>
+    // </Card.Body>
+    // </Card>
+
+    <Container fluid>
+
+    <Row className="justify-content-around" style={{marginTop: "200px"}}>
+
+    <Col xs={12} md={5} className="border"style={{minHeight: "200px", minWidth: "200px"}}>
+    <p>Hello</p>
+     <Card className="text-center col-9 border">
+     <Card.Body>
+         <Card.Title><u>{props.title}</u></Card.Title>
+         <Card.Subtitle>{props.date}</Card.Subtitle>
+         <Card.Text>{props.time}</Card.Text>
+
+         <Card.Text>{props.text}</Card.Text>
+ 
+     </Card.Body>
     </Card>
 
+    </Col>
+
+    <Col md={5} className="border"style={{minHeight: "200px", minWidth: "200px"}}>
+    <p>Hello</p>
+
+
+    </Col>
+
+
+    </Row>
+
+
+    </Container>
     )
 
 }
