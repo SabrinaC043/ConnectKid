bbce4b03096afba3c00ae6944e0b9b9355dc7b91
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-13 14:09:00 -0400

    Event Schema

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,23 +1,15 @@
 const { Schema, model } = require("mongoose");
-<<<<<<< HEAD
 // const childSchema = require("./childSchema");
-=======
->>>>>>> 1bd686409ece69b208f530f922b1e162fd7d053b
 
 const eventSchema = new Schema({
   name: { type: String, unique: true },
 
   location: { type: String },
 
-<<<<<<< HEAD
   time: { type: Number },
-=======
-    timestamps: true,
->>>>>>> 1bd686409ece69b208f530f922b1e162fd7d053b
 
   date: { type: Date },
 
-<<<<<<< HEAD
   preparationTips: { type: String },
 
   attendees: [
@@ -26,12 +18,6 @@ const eventSchema = new Schema({
       ref: "Parent",
     },
   ],
-=======
-    preparationTips: { type: String }
-
-
-
->>>>>>> 1bd686409ece69b208f530f922b1e162fd7d053b
 });
 
 const Event = model("event", eventSchema);
