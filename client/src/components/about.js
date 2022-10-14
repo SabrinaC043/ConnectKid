b5dcfd5f49997a4cdb28e0a2ba0393732d375f07
GranSpencer09 <Granspencer09@gmail.com>
2022-10-13 19:34:17 -0700

    editing about page

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -1,72 +1,72 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
+// import React from "react";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+// import Card from "react-bootstrap/Card";
 
-const About = () => {
-  return (
-    <Container fluid className="text-center">
-      <Row className="justify-content-around" style={{ marginTop: "200px" }}>
-        <Col
-          xs={12}
-          md={5}
-          className="border"
-          style={{ minHeight: "200px", minWidth: "200px" }}
-        >
-          <h2>Mission & Values</h2>
-          <h3>We're on a mission to bring families together</h3>
-          <p>
-            We value inclusion and accessibility for all kids and parents. Our
-            team seeks to provide transparent information about events local to
-            the Dallas, Texas area. We empower families to know what to expect
-            at events so they can worry less about logistics and more about
-            making friends and integrating with their local community.
-          </p>
-        </Col>
-        <Col
-          xs={12}
-          md={5}
-          className="border"
-          style={{ minHeight: "200px", minWidth: "200px" }}
-        >
-          <h2>The Connectors Team Members</h2>
-          <ul style={{ listType: "none" }}>
-            <dd>
-              <a href="https://github.com/SabrinaC043" target="_blank">
-                Sabrina Carrion
-              </a>
-            </dd>
-            <dd>
-              <a href="https://github.com/Ehren-Lewis" target="_blank">
-                Ehren Lewis
-              </a>
-            </dd>
-            <dd>
-              <a href="https://github.com/BrettMiller47" target="_blank">
-                Brett Miller
-              </a>
-            </dd>
-            <dd>
-              <a href="https://github.com/jeffbone95" target="_blank">
-                Jeffery Willingham
-              </a>
-            </dd>
-            <dd>
-              <a href="https://github.com/MikeTigray" target="_blank">
-                Micheale Abay
-              </a>
-            </dd>
-            <dd>
-              <a href="https://github.com/vianvianc" target="_blank">
-                Vian Charbonneau
-              </a>
-            </dd>
-          </ul>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+// const About = () => {
+//   return (
+//     <Container fluid className="text-center">
+//       <Row className="justify-content-around" style={{ marginTop: "200px" }}>
+//         <Col
+//           xs={12}
+//           md={5}
+//           className="border"
+//           style={{ minHeight: "200px", minWidth: "200px" }}
+//         >
+//           <h2>Mission & Values</h2>
+//           <h3>We're on a mission to bring families together</h3>
+//           <p>
+//             We value inclusion and accessibility for all kids and parents. Our
+//             team seeks to provide transparent information about events local to
+//             the Dallas, Texas area. We empower families to know what to expect
+//             at events so they can worry less about logistics and more about
+//             making friends and integrating with their local community.
+//           </p>
+//         </Col>
+//         <Col
+//           xs={12}
+//           md={5}
+//           className="border"
+//           style={{ minHeight: "200px", minWidth: "200px" }}
+//         >
+//           <h2>The Connectors Team Members</h2>
+//           <ul style={{ listType: "none" }}>
+//             <dd>
+//               <a href="https://github.com/SabrinaC043" target="_blank">
+//                 Sabrina Carrion
+//               </a>
+//             </dd>
+//             <dd>
+//               <a href="https://github.com/Ehren-Lewis" target="_blank">
+//                 Ehren Lewis
+//               </a>
+//             </dd>
+//             <dd>
+//               <a href="https://github.com/BrettMiller47" target="_blank">
+//                 Brett Miller
+//               </a>
+//             </dd>
+//             <dd>
+//               <a href="https://github.com/jeffbone95" target="_blank">
+//                 Jeffery Willingham
+//               </a>
+//             </dd>
+//             <dd>
+//               <a href="https://github.com/MikeTigray" target="_blank">
+//                 Micheale Abay
+//               </a>
+//             </dd>
+//             <dd>
+//               <a href="https://github.com/vianvianc" target="_blank">
+//                 Vian Charbonneau
+//               </a>
+//             </dd>
+//           </ul>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// };
 
-export default About;
+// export default About;
--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,13 +1,58 @@
-import React from 'react';
-import FeatureCard from '../components/cards/featureCard';
-import Sidebar from '../components/Sidebar';
+import React from "react";
+import FeatureCard from "../components/cards/featureCard";
+import BasicCard from "../components/cards/card";
+import Sidebar from "../components/Sidebar";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import Stack from "react-bootstrap/Stack";
 
 export default function Landing() {
-  
   return (
     <>
-      <h1>LANDING</h1>
-      <Sidebar /> ? how to handle calendar 'apply' button?  Do we only want Calendar on events.js?
+      {/* <h1>LANDING</h1>
+      <Sidebar /> ? how to handle calendar 'apply' button? Do we only want
+      Calendar on events.js? */}
+      <Container fluid className="text-center">
+        <Row className="align-items-center" style={{ marginTop: "100px" }}>
+          <Col>
+            <Stack gap={1}>
+              <div
+                className=""
+                xs={12}
+                md={5}
+                style={{ minHeight: "200px", minWidth: "100px" }}
+              >
+                {/* <FeatureCard />
+                
+                Area to import the FeatureCard
+
+                */}
+
+                <Card style={{ minHeight: "100px", minWidth: "200px" }}>
+                  <h2>Upcoming Virtual Zoom Cooking Class</h2>
+                  <p>Link to Zoom</p>
+                </Card>
+              </div>
+              <div>
+                <BasicCard />
+                {/* <Card style={{ minHeight: "100px", minWidth: "100px" }}>
+                  <h4>Event details</h4>
+                  <p>Ingredients:</p>
+                  <p>Tips:</p>
+                </Card> */}
+              </div>
+            </Stack>
+          </Col>
+          <Col xs={12} md={5} style={{ minHeight: "400px", minWidth: "200px" }}>
+            <Card style={{ minHeight: "400px", minWidth: "200px" }}>
+              <h3>Recommended Event</h3>
+              <p>Not a neccessary feature</p>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
       {/* <FeatureCard
         title={'title'}
         date={'date'}
