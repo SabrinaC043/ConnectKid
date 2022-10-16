c33d33b2e7c9d91a1fddc7ce50d2749f65510198
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-15 20:19:29 -0400

    added weekly model feature to landing page

--- client/src/utils/apollo/mutations.js
+++ client/src/utils/apollo/mutations.js
@@ -1,17 +1,61 @@
 import { gql } from "@apollo/client";
 
-
-
 const CREATE_PARENT = gql`
-mutation CreateParent($firstName: String!, $lastName: String!, $email: String!, $password: String!, $age: Int!) {
-  createParent(firstName: $firstName, lastName: $lastName, email: $email, password: $password, age: $age) {
-  parent {
-    firstName
-    lastName
-    email
+  mutation CreateParent(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+    $age: Int!
+  ) {
+    createParent(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+      age: $age
+    ) {
+      parent {
+        firstName
+        lastName
+        email
+      }
+      token
+    }
   }
-    token
+`;
+const CREATE_CHILD = gql`
+  mutation CreateChild(
+    $firstName: String!
+    $lastName: String!
+    $age: Int
+    $interests: [String]
+    $gender: String
+    $parentId: ID
+  ) {
+    createChild(
+      firstName: $firstName
+      lastName: $lastName
+      age: $age
+      interests: $interests
+      gender: $gender
+      parentId: $parentId
+    ) {
+      firstName
+      lastName
+      age
+      interests
+      gender
+    }
   }
-}
-`
-export  { CREATE_PARENT };
+`;
+
+// const ADD_PARENT_TO_EVENT = gql`
+// mutation AddParentToEvent($parentId: ID!, $eventId: ID!) {
+//   addParentToEvent(parentId: $parentId, eventId: $eventId) {
+
+//   }
+// }
+
+// `;
+export { CREATE_PARENT, CREATE_CHILD };
